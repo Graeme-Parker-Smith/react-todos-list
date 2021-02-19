22890878ae012ef0e2edcb987d5fc8eed5a9e02d
Graeme <graemster@gmail.com>
2021-02-19 03:32:25 -0600

    Updates

---
+++ precache-manifest.18c8f381f7b0de59f0d17514a7c94df0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b45245a18d2d5d689426e05de965cb4",
+    "url": "/react-todos-list/index.html"
+  },
+  {
+    "revision": "c5df4dad89f08dcbfb1e",
+    "url": "/react-todos-list/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9c936db12e1fae359686",
+    "url": "/react-todos-list/static/js/2.724c99d7.chunk.js"
+  },
+  {
+    "revision": "c5df4dad89f08dcbfb1e",
+    "url": "/react-todos-list/static/js/main.ca280800.chunk.js"
+  },
+  {
+    "revision": "dc201cb0043bbd4df00d",
+    "url": "/react-todos-list/static/js/runtime~main.a87ed928.js"
+  }
+]);
